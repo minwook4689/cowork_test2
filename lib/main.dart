b7440593fde2589561874258f3804be750badefb
minwook4689 <mwkim4689@gmail.com>
2025-10-14 20:04:58 +0900

    fab 수정

--- lib/main.dart
+++ lib/main.dart
@@ -114,8 +114,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'Decrease',
+        child: const Icon(Icons.deck_outlined),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
